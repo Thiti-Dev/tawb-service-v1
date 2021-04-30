b566f78fd8ce9b9bbd5d083d997196d2bd5a4aef
Thiti Dev <thiti.mwk.main@gmail.com>
2021-05-01 00:40:47 +0700

    connecting mongodb on start

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/Thiti-Dev/tawb-service-v1/database"
 	route_utils "github.com/Thiti-Dev/tawb-service-v1/routes/api/v1"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	database.ConnectMongoDatabase()
 	route_utils.Initialize_Route(router)
 	router.Run(":8080")
 }
