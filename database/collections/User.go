de45457b85df2fb05383d290e4b53f71ecc93cc3
Thiti Dev <thiti.mwk.main@gmail.com>
2021-05-01 00:39:31 +0700

    created User struct <bson,json>

---
+++ database/collections/User.go
@@ -0,0 +1,15 @@
+package collections
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID        primitive.ObjectID `bson:"_id" json:"id,omitempty"`
+	Username  string             `json:"username"`
+	Password  string             `json:"password"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at,omitempty"`
+	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at,omitempty"`
+}
