055a47e49cf03c53f01bab9705310295f8298cd2
Thiti Dev <thiti.mwk.main@gmail.com>
2021-05-01 01:43:11 +0700

    created jwt_util_pkg

---
+++ packages/jwt/jwt.go
@@ -0,0 +1,29 @@
+package jwt
+
+import (
+	"fmt"
+	"time"
+
+	models "github.com/Thiti-Dev/tawb-service-v1/models/jwt"
+	"github.com/dgrijalva/jwt-go"
+)
+
+// GetSignedTokenFromData -> is a (fn) that will sign a token with the given data
+func GetSignedTokenFromData(data models.RequiredDataToClaims) string{
+	claims := models.CustomClaims{
+		Username: data.Username,
+		ID: data.ID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+			Issuer:    "aittty.io",
+		},
+	}
+	// Create an unsigned token from the claims above
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Sign the token ->  preferably at least 256 bits in length (in-production xD)
+	signedToken, err := token.SignedString([]byte("TemporaryJustForNow"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	return signedToken
+}
