3e96eefb7ca31966a5a5e7f0e5c1abc7467e33fb
Thiti Dev <thiti.mwk.main@gmail.com>
2021-05-01 01:22:53 +0700

    runs InitializeTranslator on server start

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/Thiti-Dev/tawb-service-v1/database"
 	route_utils "github.com/Thiti-Dev/tawb-service-v1/routes/api/v1"
+	"github.com/Thiti-Dev/tawb-service-v1/validator"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	validator.InitializeTranslator() // init validator translation (CUSTOM-DECLARED)
 	database.ConnectMongoDatabase()
-	route_utils.Initialize_Route(router)
+	route_utils.Initialize_Route(router) // init routes
 	router.Run(":8080")
 }
